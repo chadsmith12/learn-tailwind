bdef13aaffe106b47024d2ec83c0e119fd42ac33
Chad Smith <smith.chad12@gmail.com>
2019-12-04 23:51:30 -0600

    customize tailwind design system with brand color and custom spacing value

--- extracting-components/tailwind.config.js
+++ extracting-components/tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                'brand-blue': {
+                    100: '#1992d4',
+                    200: '#1578ad'
+                },
+            },
+            spacing: {
+                '72': '18rem',
+            }
+        }
     },
     variants: {
         backgroundColor:  ['responsive', 'hover', 'focus' ,'active']
