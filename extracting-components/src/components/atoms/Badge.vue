4c62d03b68f5b9594f59581ba308cc21d6cfea70
Chad Smith <smith.chad12@gmail.com>
2019-12-15 11:51:26 -0600

    create a reusable badge component

---
+++ extracting-components/src/components/atoms/Badge.vue
@@ -0,0 +1,35 @@
+<template>
+    <span :class="classes" class="inline-block text-xs px-2 rounded-full font-semibold tracking-wide">
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        backgroundColor: {
+            type: String,
+            default: 'teal-200'
+        },
+        textColor: {
+            type: String,
+            default: 'teal-800'
+        },
+        uppercase: {
+            type: Boolean,
+            default: true
+        }
+    },
+    computed: {
+        classes: function() {
+            const backgroundColor = `bg-${this.backgroundColor}`;
+            const textColor = `text-${this.textColor}`;
+            const textClasses =  {
+                "uppercase": this.uppercase
+            }
+
+            return [backgroundColor, textColor, textClasses];
+        }
+    }
+}
+</script>
