32e3ecc8ec138b8665cf86d5bddfd493e002b9a7
Chad Smith <smith.chad12@gmail.com>
2019-12-15 01:44:41 -0600

    add star-rating component to the property-card

--- extracting-components/src/components/PropertyCard.vue
+++ extracting-components/src/components/PropertyCard.vue
@@ -10,9 +10,7 @@
                 {{ property.formattedPrice }} <span class="text-gray-600 text-sm">/ wk</span>
             </div>
             <div class="mt-2 flex items-center ml-2">
-            <svg v-for="i in 5" :key="i" :class="i <= property.rating ? 'text-teal-500' : 'text-gray-400'" class="h-4 w-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8.133 20.333c-1.147.628-2.488-.387-2.269-1.718l.739-4.488-3.13-3.178c-.927-.943-.415-2.585.867-2.78l4.324-.654 1.934-4.083a1.536 1.536 0 0 1 2.804 0l1.934 4.083 4.324.655c1.282.194 1.794 1.836.866 2.78l-3.129 3.177.739 4.488c.219 1.331-1.122 2.346-2.269 1.718L12 18.214l-3.867 2.119z" fill-rule="evenodd"/>
-            </svg>
+                <StarRating :value="property.rating" readonly/>
                 <span class="text-gray-600 text-sm ml-2">{{ property.reviewCount }} reviews</span>
             </div>
         </div>
@@ -20,7 +18,12 @@
 </template>
 
 <script>
+import StarRating from  "@/components/atoms/StarRating.vue";
+
 export default {
+    components: {
+        StarRating
+    },
     props: {
         property:  {
             type: Object
