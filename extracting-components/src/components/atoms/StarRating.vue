31c7d56bcbd93e98c5520057a49e4b66f16d316e
Chad Smith <smith.chad12@gmail.com>
2019-12-15 01:39:18 -0600

    add  v-model and color support to the star-rating component

--- extracting-components/src/components/atoms/StarRating.vue
+++ extracting-components/src/components/atoms/StarRating.vue
@@ -1,6 +1,11 @@
 <template>
 <span class="flex">
-    <svg v-for="i in maxNumber" :key="i" class="h-4 w-4 fill-current text-teal-400" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg v-for="i in max" :key="i"
+         :class="[...classes, currentColor(i)]"
+         @click="clickStar(i)"
+         :style="styles"
+          class="fill-current" 
+          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M8.133 20.333c-1.147.628-2.488-.387-2.269-1.718l.739-4.488-3.13-3.178c-.927-.943-.415-2.585.867-2.78l4.324-.654 1.934-4.083a1.536 1.536 0 0 1 2.804 0l1.934 4.083 4.324.655c1.282.194 1.794 1.836.866 2.78l-3.129 3.177.739 4.488c.219 1.331-1.122 2.346-2.269 1.718L12 18.214l-3.867 2.119z" fill-rule="evenodd"/>
     </svg>
 </span>
@@ -9,13 +14,64 @@
 <script>
 export default {
     props: {
-        maxNumber: {
+        max: {
             type: Number,
             default: 5
         },
         fillColor: {
             type: String,
             default: 'red'
+        },
+        size: {
+            type: [Number, String],
+            default: 4
+        },
+        color: {
+            type:  String,
+            default: 'text-red-400'
+        },
+        emptyColor: {
+            type: String,
+            default: 'text-gray-400'
+        },
+        value: {
+            type: Number,
+            default: 0
+        },
+        readonly:  {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        classes: function() {
+            const widthClass = `w-${this.size}`;
+            const heightClass  = `h-${this.size}`;
+            return [widthClass, heightClass, this.fillColor];
+        },
+        styles: function() {
+            let currentStyles = {
+                cursor: 'pointer'
+            };
+            if(this.readonly) {
+                currentStyles['pointer-events'] = 'none';
+            }
+
+            return currentStyles;
+        }
+    },
+    methods: {
+        currentColor: function(currentIndex) {
+            return currentIndex <= this.value ? this.color : this.emptyColor;
+        },
+        clickStar(currentIndex) {
+            let currentRating = currentIndex;
+            // the current rating is below the min or than the max
+            if(currentRating >= this.max) {
+                currentRating = this.max;
+            }
+
+            this.$emit('input', currentRating);
         }
     }
 }
--- extracting-components/src/views/storyboard/star-rating-board.vue
+++ extracting-components/src/views/storyboard/star-rating-board.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
         <h2 class="text-xl text-gray-800">Star Rating Component</h2>
-        <StarRating :max-number="6"/>
+        <StarRating v-model="rating" :readonly="isReadOnly"/>
+        <span>Current Rating: {{ rating }}</span>
+        <br>
+        <input type="checkbox" v-model="isReadOnly"/>
+        <span class="pl-2">Read Only?</span>
     </div>
 </template>
 <script>
 import StarRating from "@/components/atoms/StarRating.vue";
+
 export default {
     components: {
         StarRating
+    },
+    data() {
+        return {
+            rating: 3,
+            isReadOnly: false
+        };
     }
 }
 </script>
