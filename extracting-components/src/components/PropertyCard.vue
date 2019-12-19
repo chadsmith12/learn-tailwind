7679e6c61a911002a1fba4c1a17c92359deda57a
Chad Smith <smith.chad12@gmail.com>
2019-12-18 21:48:51 -0600

    add shadows and layers to property card

--- extracting-components/src/components/PropertyCard.vue
+++ extracting-components/src/components/PropertyCard.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="bg-white border rounded-lg overflow-hidden">
-        <div class="relative bg-red-500 pb-2/3">
-            <img :src="property.imageUrl" class="absolute h-full w-full object-cover" :alt="property.imageAlt">
+    <div>
+        <div class="relative bg-red-500 pb-5/6">
+            <img :src="property.imageUrl" class="absolute h-full w-full object-cover rounded-lg shadow-md" :alt="property.imageAlt">
         </div>
-        <div class="p-6">
-            <div class="flex items-baseline">
-                <Badge uppercase>New</Badge>
-                <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-                    {{ property.beds }} beds &bull; {{ property.baths }} baths
+        <div class="relative px-4 -mt-16">
+            <div class="bg-white p-6 rounded-lg shadow-lg">
+                <div class="flex items-baseline">
+                    <Badge uppercase>New</Badge>
+                    <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
+                        {{ property.beds }} beds &bull; {{ property.baths }} baths
+                    </div>
                 </div>
-            </div>
-            <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
-            <div class="mt-1">
-                {{ property.formattedPrice }} <span class="text-gray-600 text-sm">/ wk</span>
-            </div>
-            <div class="mt-2 flex items-center ml-2">
-                <StarRating :value="property.rating" readonly/>
-                <span class="text-gray-600 text-sm ml-2">{{ property.reviewCount }} reviews</span>
+                <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
+                <div class="mt-1">
+                    {{ property.formattedPrice }} <span class="text-gray-600 text-sm">/ wk</span>
+                </div>
+                <div class="mt-2 flex items-center ml-2">
+                    <StarRating :value="property.rating" readonly/>
+                    <span class="text-gray-600 text-sm ml-2">{{ property.reviewCount }} reviews</span>
+                </div>
             </div>
         </div>
     </div>
