e22664a03fe17dcc50ac115c435550e65a1a822a
Chad Smith <smith.chad12@gmail.com>
2019-12-21 17:07:53 -0600

    add basic navbar component styles

--- extracting-components/src/views/Storyboard.vue
+++ extracting-components/src/views/Storyboard.vue
@@ -1,10 +1,18 @@
 <template>
-    <div class="antialiased text-gray-900 bg-gray-200 min-h-screen p-8 flex items-center justify-center">
-        <router-view/>
+    <div>
+        <Navbar/>
+        <div class="antialiased text-gray-900 bg-gray-200 min-h-screen p-8 flex items-center justify-center">
+            <router-view/>
+        </div>
     </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue"
+
 export default {
+    components: {
+        Navbar
+    }
 }
 </script>
