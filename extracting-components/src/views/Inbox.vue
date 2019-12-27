a398c99a68dae348d7bd2f141b78494a70b82143
Chad Smith <chadsmith4@my.unt.edu>
2019-12-27 17:20:07 -0600

    add sidebar links

--- extracting-components/src/views/Inbox.vue
+++ extracting-components/src/views/Inbox.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <header class="flex">
+  <div class="h-screen flex flex-col">
+    <header class="flex flex-shrink-0">
       <div class="w-64 flex-shrink-0 px-4 py-3 bg-gray-800">
         <button class="block w-full flex items-center">
           <img
@@ -63,16 +63,108 @@
           </button>
         </div>
       </div>
-      <div></div>
     </header>
-    <div></div>
-    <main>
-      <div></div>
-      <div></div>
-    </main>
+    <div class="flex-1 flex">
+      <div class="p-6 w-64 bg-gray-100">
+        <nav>
+          <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wide">Mailboxes</h2>
+          <div class="mt-3">
+            <a
+              href="#"
+              class="-mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path
+                    d="M18 2a2 2 0 012 2v12a2 2 0 01-2 2H2a2 2 0 01-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z"
+                  />
+                </svg>
+                <span class="ml-2 text-gray-900">Inbox</span>
+              </span>
+              <Badge background-color="gray-200" text-color="gray-700">6</Badge>
+            </a>
+            <a
+              href="#"
+              class="mt-2 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path d="M7.667 12H2v8H0V0h12l.333 2H20l-3 6 3 6H8l-.333-2z" />
+                </svg>
+                <span class="ml-2 text-gray-900">Flagged</span>
+              </span>
+            </a>
+            <a
+              href="#"
+              class="mt-2 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path
+                    d="M2 4v14h14v-6l2-2v10H0V2h10L8 4H2zm10.3-.3l4 4L8 16H4v-4l8.3-8.3zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"
+                  />
+                </svg>
+                <span class="ml-2 text-gray-900">Drafts</span>
+              </span>
+              <Badge background-color="gray-200" text-color="gray-700">2</Badge>
+            </a>
+            <a
+              href="#"
+              class="mt-2 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path
+                    d="M10 20a10 10 0 110-20 10 10 0 010 20zM7 6v2a3 3 0 106 0V6a3 3 0 10-6 0zm-3.65 8.44a8 8 0 0013.3 0 15.94 15.94 0 00-13.3 0z"
+                  />
+                </svg>
+                <span class="ml-2 text-gray-900">Assigned</span>
+              </span>
+              <Badge background-color="gray-200" text-color="gray-700">1</Badge>
+            </a>
+            <a
+              href="#"
+              class="mt-2 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path
+                    d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"
+                  />
+                </svg>
+                <span class="ml-2 text-gray-900">Closed</span>
+              </span>
+            </a>
+            <a
+              href="#"
+              class="mt-2 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg"
+            >
+              <span class="inline-flex">
+                <svg class="h-5 w-5 fill-current text-gray-700" viewBox="0 0 20 20">
+                  <path
+                    d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"
+                  />
+                </svg>
+                <span class="ml-2 text-gray-900">Junk</span>
+              </span>
+            </a>
+          </div>
+        </nav>
+      </div>
+      <main class="bg-gray-200">
+        <div></div>
+        <div></div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Badge from "@/components/atoms/Badge.vue";
+
+export default {
+  components: {
+    Badge
+  }
+};
 </script>
